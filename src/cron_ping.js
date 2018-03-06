5e9396f05e9134b3d5ca4e4248df2d7e74c97c63
cheneyweb <457299596@qq.com>
2018-03-06 17:20:27 +0800

    增加定时心跳

--- src/cron_ping.js
+++ src/cron_ping.js
@@ -3,5 +3,6 @@ const axios = require('axios')
 
 // 心跳激活
 cron.schedule('*/30 * * * * *', function () {
-    axios.get('https://dl57vasdud645.cloudfront.net/ping')
+    axios.get('https://dl57vasdud645.cloudfront.net/ping')  // 云LOG服务
+    axios.get('https://d3oo6v0d877qm6.cloudfront.net/ping') // 测试代理
 })
