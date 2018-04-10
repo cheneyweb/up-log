23ffdd427f74321f0be96e50f8b9f50ac6fd14fa
cheneyweb <457299596@qq.com>
2018-04-10 13:18:37 +0800

    文档

--- src/cron_ping.js
+++ src/cron_ping.js
@@ -5,6 +5,6 @@ const axios = require('axios')
 cron.schedule('*/30 * * * * *', function () {
     axios.get('https://dl57vasdud645.cloudfront.net/ping')  // 云LOG服务
     axios.get('https://d3oo6v0d877qm6.cloudfront.net/ping') // 测试代理
-    axios.get('https://d3fb95uaptbs2t.cloudfront.net/ping') // 正式代理
-    axios.get('https://d7nnwhk3clfz0.cloudfront.net/ping')  // N2代理
+    axios.get('https://n1agent.na12345.com/ping') // N1代理
+    axios.get('https://n2agent.na12345.com/ping') // N2代理
 })
