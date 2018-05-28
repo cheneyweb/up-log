cf7501000561214b0cee7b2befa854fc2f683f1f
cheneyweb <457299596@qq.com>
2018-05-28 10:12:50 +0800

    心跳接口

--- src/cron_ping.js
+++ src/cron_ping.js
@@ -4,10 +4,12 @@ const axios = require('axios')
 // 心跳激活
 cron.schedule('*/30 * * * * *', function () {
     axios.get('https://dl57vasdud645.cloudfront.net/ping')  // 云LOG服务
+
     axios.get('https://d3oo6v0d877qm6.cloudfront.net/ping') // N1测试代理
     axios.get('https://d3rqtlfdd4m9wd.cloudfront.net/ping') // N1测试管理员
     axios.get('https://dgjmusja39cm2.cloudfront.net/ping')  // N1测试游戏
-    axios.get('https://n1agent.na12345.com/ping') // N1代理
+    
+    // axios.get('https://n1agent.na12345.com/ping') // N1代理
     axios.get('https://n2agent.na12345.com/ping') // N2代理
     axios.get('https://n1admin.na12345.com/ping') // N1管理员
     axios.get('https://n1game.na12345.com/ping')  // N1游戏
