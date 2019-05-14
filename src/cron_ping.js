142560cf8592ac382a3005f6422a09955049ba7d
cheneyweb <457299596@qq.com>
2019-05-14 15:35:43 +0800

    优化

--- src/cron_ping.js
+++ src/cron_ping.js
@@ -11,6 +11,6 @@ cron.schedule('*/30 * * * * *', function () {
     
     axios.get('https://n1agent.na12345.com/ping') // N1代理
     axios.get('https://n1admin.na12345.com/ping') // N1管理员
-    axios.get('https://n1game.na12345.com/ping')  // N1游戏
+    // axios.get('https://n1game.na12345.com/ping')  // N1游戏
     // axios.get('https://n2agent.na12345.com/ping') // N2代理
 })
